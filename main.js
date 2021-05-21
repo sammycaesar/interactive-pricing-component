8cec13c875a8470b72fdd8a1a4d7e23ba7c69667
sammycaesar <samantha.caesar@bigpond.com>
2021-05-21 15:35:33 +0200

    Updated JS file and started CSS on mobile view

--- main.js
+++ main.js
@@ -4,14 +4,64 @@
 let slider = document.getElementById("range");
 let price = document.getElementById("price");
 let pageViews = document.getElementById("pageviews");
-const discount = 0.25;
+let toggle = document.getElementById("switch");
+let fullPrice = document.getElementById("full-price");
+let discountPrice = document.getElementById("discount-price");
+let toggleWrapper = document.querySelector(".switch-wrapper");
 
 // Data
-let data = [
-  { pageViews: 10, price: 8 },
-  { pageViews: 50, price: 12 },
-  { pageViews: 100, price: 16 },
-  { pageViews: 100, price: 16 },
-  { pageViews: 500, price: 24 },
-  { pageViews: 1, price: 36 },
-];
+let cost = [8, 12, 16, 24, 36];
+let views = ["10K", "50K", "100K", "500K", "1M"];
+
+//Initial Values on Page Load
+price.innerHTML = "16.00";
+pageViews.innerHTML = "100K";
+slider.value = 3;
+
+toggle.addEventListener("click", () => {
+  price.classList.toggle("hidden");
+  discountPrice.classList.toggle("hidden");
+  toggleWrapper.classList.toggle("active");
+});
+
+// Discount
+function addDiscount(intialPrice) {
+  return intialPrice - (intialPrice * 25) / 100;
+}
+
+// Show values when moving the interactive slider
+slider.oninput = function () {
+  const inputValue = slider.value;
+  switch (inputValue) {
+    case "1": {
+      price.innerHTML = Number(cost[0]).toFixed(2);
+      pageViews.innerHTML = "10K";
+      discountPrice.innerHTML = addDiscount(cost[0]).toFixed(2);
+      break;
+    }
+    case "2": {
+      price.innerHTML = Number(cost[1]).toFixed(2);
+      pageViews.innerHTML = "50K";
+      discountPrice.innerHTML = addDiscount(cost[1]).toFixed(2);
+      break;
+    }
+    case "3": {
+      price.innerHTML = Number(cost[2]).toFixed(2);
+      pageViews.innerHTML = "100K";
+      discountPrice.innerHTML = addDiscount(cost[2]).toFixed(2);
+      break;
+    }
+    case "4": {
+      price.innerHTML = Number(cost[3]).toFixed(2);
+      pageViews.innerHTML = "500K";
+      discountPrice.innerHTML = addDiscount(cost[3]).toFixed(2);
+      break;
+    }
+    case "5": {
+      price.innerHTML = Number(cost[4]).toFixed(2);
+      pageViews.innerHTML = "1M";
+      discountPrice.innerHTML = addDiscount(cost[4]).toFixed(2);
+      break;
+    }
+  }
+};
